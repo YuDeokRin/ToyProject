8ea9c53d95e87098e767c21e74f048756369cbcb
YuDeokRin <ejrfls12@naver.com>
2022-06-20 17:02:34 +0900

    6. GUI로 주소입력해서 주소찾기 만들기

---
+++ src/project01/Project01_F.java
@@ -0,0 +1,74 @@
+package project01;
+
+import project01.naver.NaverMap;
+
+import javax.swing.*;
+import java.awt.*;
+
+/*
+
+ GUI로 주소입력해서 주소찾기 만들기
+
+    1.GUI에서 주소입력 부분에서 입력 후 -> 검색 부분을 누른다.
+    2. openAPI를 이용해서 naver 서버에서 요청을 한다. -> (geocoding) 요청
+    3. 위도, 경도의 정보를 받는다. -> geocoding을 통해서 위도, 경도의 정보를 제공받는다.
+    4. 위도, 경도를 받은 정보를 static map이라는 지도를 이미지파일로 만들어준다.
+    5. 이미지 파일을 GUI에서 지도 보여주는 부분에 보여준다.
+    6. 하단에는 도로명, 지번주소, 경도, 위도를 알려준다.
+ */
+public class Project01_F {
+    public JTextField address;
+    public JLabel resAddress;
+    public JLabel resX;
+    public JLabel resY;
+    public JLabel jibunAddress;
+
+    public JLabel imageLabel;
+
+    public void initGUI() {
+        //frame 과 Container  그리고 종료 버튼
+        JFrame frm = new JFrame("Map View"); //GUI 전체 프레임 객체 생성
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 종료 버튼이 생기고 누르면 종료되는 것
+        Container c = frm.getContentPane();  // JFrame 안에서 컨테이너를 가져오기 위해서 getContentPane();을 쓴다.
+
+        //중간에 지도보기 JLabel
+        imageLabel = new JLabel("지도보기"); // 지도 이미지들어 갈 객체 생성
+
+
+        JPanel pan = new JPanel(); // JPanel 객체생성  상단(주소입력, 주소쓰는곳, 검색버튼) 들어가는 패널
+        //컨테이너 안에 상단의 위치한 주소입력, JTextField, 검색버튼
+        JLabel addressLbl = new JLabel("주소입력");
+        address = new JTextField(50);
+        JButton btn = new JButton("검색");
+
+        //위의 것들을 패널에 넣어준다.
+        pan.add(addressLbl); // 패널에 넣어준다.
+        pan.add(address); // 패널에 넣어준다.
+        pan.add(btn); // 패널에 넣어준다.
+        btn.addActionListener(new NaverMap(this)); // 검색 버튼을 누르면 ->  NaverMap(this)를
+
+        JPanel pan1 = new JPanel(); // JPanel 객체생성 -> 하단( 도로명, 지번주소, 경도, 위도) 들어가는 패널
+        pan1.setLayout(new GridLayout(4, 1));//  패널은 GridLayout으로 4행 1열
+        resAddress = new JLabel("도로명");
+        jibunAddress = new JLabel("지번주소");
+        resX = new JLabel("경도");
+        resY = new JLabel("위도");
+
+        //위의 것들을 패널에 넣어준다.
+        pan1.add(resAddress); // 패널에 넣어준다.
+        pan1.add(jibunAddress);// 패널에 넣어준다.
+        pan1.add(resX);// 패널에 넣어준다.
+        pan1.add(resY);// 패널에 넣어준다.
+
+        //마지막으로 pan, imageLabel,pan1을
+        c.add(BorderLayout.NORTH, pan); //상단
+        c.add(BorderLayout.CENTER, imageLabel); // 중간
+        c.add(BorderLayout.SOUTH, pan1); // 하단
+        frm.setSize(800, 660); // frame size 넣기 (width(가로), height(세로))
+        frm.setVisible(true); // 화면 뜨게하는 것
+    }
+    public static void main(String[] args) {
+        new Project01_F().initGUI();
+
+    }
+}
---
+++ src/project01/naver/NaverMap.java
@@ -0,0 +1,133 @@
+package project01.naver;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import project01.addressVO.AddressVO;
+import project01.Project01_F;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Date;
+
+public class NaverMap implements ActionListener { // Button이 눌렀을 때, 실행되야하므로 implements를 해준다.
+    Project01_F naverMap;
+    public NaverMap(Project01_F naverMap) { // this 객체를 받아서 naverMap에 넣어준다.
+        this.naverMap = naverMap;
+    }
+
+    //actionPerformed 구현부분이 위도와 경도를 가져오는 부분이다.
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String clientId = "yh1oswg56e";
+        String clientSecret = "cXPkfCfTFfgEMajDhSZaP0FPnxy9fwWNr3UfcvpK";
+        AddressVO vo = null;
+
+        try {
+            String address = naverMap.address.getText(); // 주소객체 address에 getText()한다.
+            String addr = URLEncoder.encode(address, "UTF-8");
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + addr; // geocoding에 필요한 openapi
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) { // 응답이 잘 왔을 경우
+                br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+
+            }else{ // 응답이 잘 안왔을 경우
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) { // 200을 성공하면 while문을 반복하면서 Json코드를 가져온다.
+                response.append(inputLine);
+            }
+
+            br.close();
+
+            //JSONTokener를 이용해서 문자열 데이터를 메모리에 올려준다.
+            JSONTokener tokener = new JSONTokener(response.toString());
+            JSONObject object = new JSONObject(tokener);
+            System.out.println(object);
+
+
+            //for문으로 JSON안에 있는 위도, 경도, 도로주소, 지번주소를 가져온다.
+            JSONArray arr = object.getJSONArray("addresses");
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject temp = (JSONObject) arr.get(i);
+                vo = new AddressVO(); // AddressVO에서  public AddressVO() {  } <- 디폴트 생성자 안해주면 오류뜬다.
+                vo.setRoadAddress((String)temp.get("roadAddress")); // vo에 roadAddressd 변수에 주소를 넣는다.
+                vo.setJibunAddress((String)temp.get("jibunAddress")); // vo에 jibunAddress 변수에 지번주소를 넣는다.
+                vo.setX((String) temp.get("x")); // 경도를 vo에 넣는다
+                vo.setY((String) temp.get("y")); // 위도를 vo에 넣는다.
+                System.out.println(vo);
+            }
+
+            //지도의 정보를 받아서 이미지로 변환하는 것
+            //지도 이미지 생성 메서드
+            //주소의 위치를 이미지로 생성하는 메소드
+            map_service(vo);
+
+        } catch (Exception err) {
+            System.out.println(err);
+        }
+    }
+
+    public void map_service(AddressVO vo) {
+        String URL_STATICMAP = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?";
+
+        try {
+            String pos = URLEncoder.encode(vo.getX() + " " + vo.getY(), "UTF-8");
+            URL_STATICMAP += "center=" + vo.getX() + "," + vo.getY();
+            URL_STATICMAP += "&level=16&w=700&h=500";
+            URL_STATICMAP += "&markers=type:t|size:mid|pos:" + pos + "|label:" + URLEncoder.encode(vo.getRoadAddress(), "UTF-8");
+            URL url = new URL(URL_STATICMAP);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection(); //연결 시도
+            con.setRequestMethod("GET"); //GET방식 요청
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", "yh1oswg56e");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", "cXPkfCfTFfgEMajDhSZaP0FPnxy9fwWNr3UfcvpK");
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) { // 정상적으로 작동될 때
+                InputStream is = con.getInputStream();
+                int read = 0;
+                byte[] bytes = new byte[1024];
+                String tempname = Long.valueOf(new Date().getTime()).toString();
+
+                File f = new File(tempname + ".jpg"); // 다운로드 될 이미지 생성
+                f.createNewFile(); // 파일 생성
+                OutputStream outputStream = new FileOutputStream(f);
+
+                //지도 이미지 생성
+                while ((read = is.read(bytes)) != -1) {// 서버에서 JSON데이터를 outputStream을 이용해서 jpg파일에 저장
+                    outputStream.write(bytes, 0, read);
+                }
+                is.close();
+
+                //파일로 만들어진 이미지를 f.getName의 이름으로 객체 생성
+                ImageIcon img = new ImageIcon(f.getName());
+                naverMap.imageLabel.setIcon(img);
+                naverMap.resAddress.setText(vo.getRoadAddress());
+                naverMap.jibunAddress.setText(vo.getJibunAddress());
+                naverMap.resX.setText(vo.getX());
+                naverMap.resY.setText(vo.getY());
+
+            }else{
+                System.out.println(responseCode);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+}
